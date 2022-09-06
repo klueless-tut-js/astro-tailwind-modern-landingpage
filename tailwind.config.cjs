6263d2b96f49fbc0f1ef654a7056bf7ae2f1bd0a
David Cruwys <david@ideasmen.com.au>
2022-09-06 18:04:23 +1000

    refresh

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      maxWidth: {
+        '16': '16rem',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
